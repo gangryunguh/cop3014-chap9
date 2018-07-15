8146c20fd2abd0cebc96e5b354ddffe9137dbc5e
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-07-15 15:53:54 -0500

    chap9 sample C++ source

---
+++ app/Pr9-1.cpp
@@ -0,0 +1,30 @@
+// This program uses the & operator to determine a variable’s
+// address and the sizeof operator to determine its size.
+//
+// revised by Gang-Ryung Uh to demonstrate the difference between
+//      reference variable and pointer variable
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int x = 25;
+   int &refY = x;
+   int *ptrX;
+
+   ptrX = &x;
+   cout << "The address of x is " << &x << endl;
+   cout << "The address of ptrX is " << &ptrX << endl;
+   cout << "The size of x is " << sizeof(x) << " bytes\n";
+   cout << "The value in x is " << x << endl;
+
+   cout << endl << endl;
+
+   cout << "The address of x is " << &x << endl;
+   cout << "The address of refY is " << &refY << endl;
+   cout << "The size of x is " << sizeof(x) << " bytes\n";
+   cout << "The value in x is " << x << endl;
+
+   return 0;
+}
