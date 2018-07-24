9194fa8d55b93fac291ecaddb6b56a512996eff6
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-07-24 11:15:44 -0500

    remove the body of Pr9-13.cpp

--- app/Pr9-13.cpp
+++ app/Pr9-13.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void displayValues(const int *, int);
+void displayValues(int *, int);
 
 int main()
 {
@@ -10,7 +10,7 @@ int main()
    const int SIZE = 6;
 
    // Define an array of const ints.
-   const int array1[SIZE] = { 1, 2, 3, 4, 5, 6 };
+   int array1[SIZE] = { 1, 2, 3, 4, 5, 6 };
 
    // Define an array of non-const ints.
    int array2[SIZE] = { 2, 4, 6, 8, 10, 12 };
@@ -28,12 +28,7 @@ int main()
 // parameter to display the contents of an array.   *
 //***************************************************
 
-void displayValues(const int *numbers, int size)
+void displayValues(int *numbers, int size)
 {
-   // Display all the values.
-   for (int count = 0; count < size; count++)
-   {
-      cout << *(numbers + count) << " ";
-   }
-   cout << endl;
+   // Display all the values using the pointer arithmetic
 }
